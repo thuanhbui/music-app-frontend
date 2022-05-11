4b782dfafebabf6f498a1bc2dc6947fb18eb20ac
thuanhbui <buithulop5ccx@gmail.com>
2022-05-11 21:48:30 +0700

    post user

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,7 +1,21 @@
+import {useState} from "react";
+import API from "../api"
+
 export function Input() {
 
+    const [name, setName] = useState();
+    const [age, setAge] = useState();
+
     const submit = () => {
-        console.log("post user")
+        const user = {
+            name: name,
+            age: age,
+        }
+        API.postUser(user).then((res) => {
+            console.log(res)
+        }).catch((err) => {
+            console.log(err)
+        });
     }
 
     return (
@@ -9,11 +23,12 @@ export function Input() {
             <div className="form-group">
                 <label htmlFor="exampleInputEmail1">Name</label>
                 <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-                       placeholder="Enter your name"/>
+                       placeholder="Enter your name" onChange={(e) => setName(e.target.value)}/>
             </div>
             <div className="form-group my-1">
                 <label htmlFor="exampleInputPassword1">Age</label>
-                <input type="text" className="form-control" id="exampleInputPassword1" placeholder="Enter your age"/>
+                <input type="text" className="form-control" id="exampleInputPassword1" placeholder="Enter your age"
+                onChange={(e) => setAge(e.target.value)}/>
             </div>
             <button type="submit" className="btn btn-primary mt-2" onSubmit={submit}>Submit</button>
         </form>
